1f86a10b77e1ac0f6bed27bcf19f4dbc376fa497
YC.Huang <271yeye@gmail.com>
2025-02-04 15:53:02 +0800

    feat: connect to OpenSearch

---
+++ backend/connect_OpenSearch_collection.py
@@ -0,0 +1,29 @@
+# store these vectors so they can be searched and retrieved efficiently. To do so, we can use a vector database.
+import boto3
+# Import required libraries to connect to Amazon OpenSearch Serverless connection
+from opensearchpy import OpenSearch, RequestsHttpConnection, AWSV4SignerAuth
+from requests_aws4auth import AWS4Auth
+
+# Initialize endpoint name constant
+HOST = "abc.us-west-2.aoss.amazonaws.com" # OpenSearch endpoint. For example, abcdefghi.us-east-1.aoss.amazonaws.com (without https://)
+REGION = "us-west-2" # OpenSearch region
+service = 'aoss' # OpenSearch service name
+
+# connect to your Amazon OpenSearch Serverless collection.
+def initialize_opensearch_client():
+    
+    # Initialize and authenticate with the OpenSearch client
+    credentials = boto3.Session().get_credentials()
+    awsauth = AWS4Auth(credentials.access_key, credentials.secret_key, REGION, 'aoss', session_token=credentials.token)
+    auth = AWSV4SignerAuth(credentials, REGION, service)
+
+    client = OpenSearch(
+        hosts=[{'host': HOST, 'port': 443}],
+        http_auth=auth,
+        use_ssl=True,
+        verify_certs=True,
+        connection_class=RequestsHttpConnection,
+        pool_maxsize=300
+    )
+    return client
+
